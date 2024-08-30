2dedc8cc5a5cb12ba809ba4ac304a5a6c5472c3a
gfunk77 <faraci77@gmail.com>
2024-08-30 08:03:48 -0400

    add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
+import { ListingsComponent } from './listings/listings.component';
+import { ListingDetailComponent } from './listing-detail/listing-detail.component';
+import { EditListingComponent } from './edit-listing/edit-listing.component';
+import { ContactComponent } from './contact/contact.component';
+import { MyListingsComponent } from './my-listings/my-listings.component';
+import { NewListingComponent } from './new-listing/new-listing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listings',
+    pathMatch: 'full',
+  },
+  {
+    path: 'listings',
+    component: ListingsComponent,
+    title: 'Listings',
+  },
+  {
+    path: 'listing/:id',
+    component: ListingDetailComponent,
+    title: 'Listing Detail',
+  },
+  {
+    path: 'listing/edit/:id',
+    component: EditListingComponent,
+    title: 'Edit Listing',
+  },
+  {
+    path: 'contact/:id',
+    component: ContactComponent,
+    title: 'Contact',
+  },
+  {
+    path: 'my-listings',
+    component: MyListingsComponent,
+    title: 'My Listings',
+  },
+  {
+    path: 'new-listing',
+    component: NewListingComponent,
+    title: 'New Listing',
+  },
+  {
+    path: '**',
+    redirectTo: 'listings',
+    pathMatch: 'full',
+  },
+];
