0a44f0487751161d185799b2eb5c8c9e8ce7aac3
gfunk77 <faraci77@gmail.com>
2024-08-30 07:56:41 -0400

    add components

---
+++ client/src/app/my-listings/my-listings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-listings',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-listings.component.html',
+  styleUrl: './my-listings.component.scss'
+})
+export class MyListingsComponent {
+
+}
---
+++ client/src/app/new-listing/new-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-listing.component.html',
+  styleUrl: './new-listing.component.scss'
+})
+export class NewListingComponent {
+
+}
