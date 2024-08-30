8f9150c18b9622451bbe314906a28842dfc2f236
gfunk77 <faraci77@gmail.com>
2024-08-30 13:00:50 -0400

    listing detail

--- client/src/app/listing-detail/listing-detail.component.ts
+++ client/src/app/listing-detail/listing-detail.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Listing } from '../models/listing';
+import { fakeData } from '../fakeData';
 
 @Component({
   selector: 'app-listing-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './listing-detail.component.html',
-  styleUrl: './listing-detail.component.scss'
+  styleUrl: './listing-detail.component.scss',
 })
-export class ListingDetailComponent {
+export class ListingDetailComponent implements OnInit {
+  router: Router = inject(Router);
+  route: ActivatedRoute = inject(ActivatedRoute);
+  listing!: Listing;
 
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const item = fakeData.find((listing) => listing.id === id);
+    if (item) {
+      this.listing = item;
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/listings']);
+  }
+
+  toContact(id: number) {
+    this.router.navigate(['/contact', id]);
+  }
 }
