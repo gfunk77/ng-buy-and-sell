bc9c8cd54e1d5fe3d8703d74ed762712941ebd78
gfunk77 <faraci77@gmail.com>
2024-08-30 10:42:33 -0400

    nav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenu, MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  breakpointObserver = inject(BreakpointObserver);
+  title = 'Buy and Sell';
+  links = [
+    { path: '/listings', title: 'listings', icon: 'shop' },
+    { path: 'my-listings', title: 'my listings', icon: 'sell' },
+  ];
+  isScreenSmall = false;
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((result) => {
+        this.isScreenSmall = result.matches;
+      });
+  }
 }
