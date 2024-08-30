f3687e79e2459b2aaafab0b9eabd7a869d5a682a
gfunk77 <faraci77@gmail.com>
2024-08-30 12:32:16 -0400

    listing page

---
+++ client/src/app/fakeData.ts
@@ -0,0 +1,62 @@
+export const fakeData = [
+  {
+    id: 1,
+    name: 'Boat',
+    description: 'Floats on water',
+    price: 700,
+  },
+  {
+    id: 2,
+    name: 'Car',
+    description: 'Drives on land',
+    price: 1000,
+  },
+  {
+    id: 3,
+    name: 'Plane',
+    description: 'Flies in the sky',
+    price: 1600,
+  },
+  {
+    id: 4,
+    name: 'Bike',
+    description: 'Rides on roads',
+    price: 200,
+  },
+  {
+    id: 5,
+    name: 'Train',
+    description: 'Runs on tracks',
+    price: 500,
+  },
+  {
+    id: 6,
+    name: 'Bus',
+    description: 'Carries people',
+    price: 300,
+  },
+  {
+    id: 7,
+    name: 'Truck',
+    description: 'Carries goods',
+    price: 400,
+  },
+  {
+    id: 8,
+    name: 'Scooter',
+    description: 'Small bike',
+    price: 100,
+  },
+  {
+    id: 9,
+    name: 'Helicopter',
+    description: 'Flies in the sky',
+    price: 800,
+  },
+  {
+    id: 10,
+    name: 'Submarine',
+    description: 'Floats under water',
+    price: 900,
+  },
+];
--- client/src/app/listings/listings.component.ts
+++ client/src/app/listings/listings.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fakeData } from '../fakeData';
+import { Listing } from '../models/listing';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterLink],
   templateUrl: './listings.component.html',
-  styleUrl: './listings.component.scss'
+  styleUrl: './listings.component.scss',
 })
-export class ListingsComponent {
+export class ListingsComponent implements OnInit {
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeData;
+  }
 }
