ee0146c31cd7638689aae4d5f8c3a91948a47436
gfunk77 <faraci77@gmail.com>
2024-08-31 06:04:55 -0400

    new listing form

--- client/src/app/new-listing/new-listing.component.ts
+++ client/src/app/new-listing/new-listing.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './new-listing.component.html',
-  styleUrl: './new-listing.component.scss'
+  styleUrl: './new-listing.component.scss',
 })
-export class NewListingComponent {
+export class NewListingComponent implements OnInit {
+  fb: FormBuilder = inject(FormBuilder);
+  router: Router = inject(Router);
 
+  newListingForm = this.fb.group({
+    id: [null],
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    price: [null, Validators.required],
+  });
+
+  ngOnInit(): void {}
+
+  getErrorMessage(controlName: string): string {
+    return `${
+      controlName.at(0)?.toUpperCase() + controlName.slice(1)
+    } is required`;
+  }
+
+  onSubmit(): void {
+    if (this.newListingForm.invalid) {
+      return;
+    }
+    console.log(this.newListingForm.value);
+    this.router.navigate(['/my-listings']);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/my-listings']);
+  }
 }
