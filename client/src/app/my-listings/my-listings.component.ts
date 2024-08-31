bdeee076b40c95bc73e88d716cb8b6515ae8e034
gfunk77 <faraci77@gmail.com>
2024-08-31 05:19:24 -0400

    my listings page

--- client/src/app/my-listings/my-listings.component.ts
+++ client/src/app/my-listings/my-listings.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { fakeData } from '../fakeData';
+import { Listing } from '../models/listing';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-my-listings',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './my-listings.component.html',
-  styleUrl: './my-listings.component.scss'
+  styleUrl: './my-listings.component.scss',
 })
-export class MyListingsComponent {
+export class MyListingsComponent implements OnInit {
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeData;
+  }
 }
